6eb82ad0029941407b773a04d919f36515e12517
Hussain, Shah <ts-shah.hussain@rakuten.com>
2021-09-06 09:07:15 +0100

    dead code removal

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
